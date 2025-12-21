d3df012439e9a9139771ca740d3f29782a45db6b
areeb193 <fa22-bse-193@cuilahore.edu.pk>
2025-12-21 18:18:42 +0500

    aa

--- src/app/backendadmin/search/page.tsx
+++ src/app/backendadmin/search/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
